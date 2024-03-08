066aabcc94422b6c6f9e9189a109d27b6a83f59c
Vaibhav <vaibhav@parcelhero.in>
2024-03-08 21:43:35 +0530

    Working Docker Images - Credits Adrian

--- services-list.cmd
+++ services-list.cmd
@@ -7,24 +7,36 @@ REM SET DOCKER_DEVSVC_COMPOSE_FILES=^
  REM -f compose-rabbitmq.yml^
  REM -f compose-vault.yml^
  REM -f network.yml
- 
+
  REM **** WILLOW ****
-SET DOCKER_DEVSVC_PROJECT=devsvc
-SET DOCKER_DEVSVC_COMPOSE_FILES=^
- -f compose-elasticsearch.yml^
- -f compose-eventstore.yml^
- -f compose-mongo.yml^
- -f compose-rabbitmq.yml^
- -f network.yml
- 
+REM SET DOCKER_DEVSVC_PROJECT=devsvc
+REM SET DOCKER_DEVSVC_COMPOSE_FILES=^
+ REM -f compose-eventstore.yml^
+ REM -f compose-rabbitmq.yml^
+ REM -f compose-mongo.yml^
+ REM -f network.yml
+
  REM **** POPLAR ****
 REM SET DOCKER_DEVSVC_PROJECT=devsvc
 REM SET DOCKER_DEVSVC_COMPOSE_FILES=^
- REM -f compose-elasticsearch.yml^
  REM -f compose-eventstore.yml^
+ REM -f compose-eventstore-cloud.yml^
  REM -f compose-rabbitmq.yml^
  REM -f compose-postgres.yml^
- REM -f network.yml 
+ REM -f compose-mongo.yml^
+ REM -f network.yml
+
+SET DOCKER_DEVSVC_PROJECT=devsvc
+SET DOCKER_DEVSVC_COMPOSE_FILES=^
+ -f compose-eventstore.yml^
+ -f compose-eventstore-cloud.yml^
+ -f compose-rabbitmq.yml^
+ -f compose-redis.yml^
+ -f compose-postgis.yml^
+ -f compose-mongo.yml^
+ -f compose-sqlserver.yml^
+ -f network.yml
+
 
  REM **** ROWAN ****
 REM SET DOCKER_DEVSVC_PROJECT=devsvc
@@ -32,6 +44,13 @@ REM SET DOCKER_DEVSVC_COMPOSE_FILES=^
  REM -f compose-elasticsearch.yml^
  REM -f compose-rabbitmq.yml^
  REM -f compose-postgres.yml^
+ REM -f network.yml
+
+ REM **** UTILE ****
+REM SET DOCKER_DEVSVC_PROJECT=devsvc
+REM SET DOCKER_DEVSVC_COMPOSE_FILES=^
+ REM -f compose-eventstore.yml^
+ REM -f compose-mongo.yml^
  REM -f network.yml
 
 REM Add to the list above as required
@@ -45,3 +64,10 @@ REM -f compose-rabbitmq.yml^
 REM -f compose-redis.yml^
 REM -f compose-sqlserver.yml^
 REM -f compose-vault.yml^
+
+
+REM SET DOCKER_DEVSVC_PROJECT=devsvc
+REM SET DOCKER_DEVSVC_COMPOSE_FILES=^
+ REM -f compose-rabbitmq.yml^
+ REM -f compose-postgres.yml^
+ REM -f network.yml
